774794fb4f33186822f2a6a4df1b36f691c7f6f5
uriartej <uriarte.juan.j@gmail.com>
2024-01-12 15:59:51 -0800

    added files

---
+++ backend/dist/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+app.post("/hello", (req, res, next) => {
+    console.log(req.body.name);
+    return res.send("Hello");
+});
+app.listen(5000, () => console.log("Server Open"));
+//# sourceMappingURL=index.js.map
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,12 @@
-console.log(
-  "This is a starter kit for this amazing project. With 💓 By Indian Coders"
-);
+import express from "express"
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/hello", (req,res,next) => {
+  console.log(req.body.name);
+  return res.send("Hello");
+});
+
+app.listen(5000, () => console.log("Server Open"));

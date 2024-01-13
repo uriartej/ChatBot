6ab8193fec4f14d5392178b753a297bed0b78f32
uriartej <uriarte.juan.j@gmail.com>
2024-01-12 18:17:38 -0800

    got api keys form openai

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,13 @@
+import { error } from "console";
 import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
 
 //connections and listeners
-app.listen(5000, () => console.log("Server Open"));
+const PORT = process.env.PORT || 5000;
+connectToDatabase()
+.then(() => {
+    app.listen(PORT, () => 
+      console.log("Server Open and connected to the Database")
+      );
+  })
+  .catch((error) => console.log(error));
